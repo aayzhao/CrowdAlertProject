20ee6e19864a19c5429a950e0b33ca24572cf9b6
Aaron Zhao <aaron.y.zhao1@gmail.com>
2023-10-28 12:36:11 -0400

    server tests
    Signed-off-by: Aaron Zhao <aaron.y.zhao1@gmail.com>

--- src/ServerEngine.java
+++ src/ServerEngine.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Server {
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class ServerEngine {
+    public static void connectToServer() {
+        //Try to connect to the server on an unused port e.g. 9991. A successful connection will return a socket
+        try(ServerSocket serverSocket = new ServerSocket(9991)) {
+            Socket connectionSocket = serverSocket.accept();
+
+            //Create Input&Outputstreams for the connection
+            InputStream inputToServer = connectionSocket.getInputStream();
+            OutputStream outputFromServer = connectionSocket.getOutputStream();
+
+            Scanner scanner = new Scanner(inputToServer, StandardCharsets.UTF_8);
+            PrintWriter serverPrintOut = new PrintWriter(new OutputStreamWriter(outputFromServer, StandardCharsets.UTF_8), true);
+
+            serverPrintOut.println("Hello World! Enter Peace to exit.");
+
+            //Have the server take input from the client and echo it back
+            //This should be placed in a loop that listens for a terminator text e.g. bye
+            boolean done = false;
+
+            while(!done && scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                serverPrintOut.println("Echo from <Your Name Here> Server: " + line);
+
+                if(line.toLowerCase().trim().equals("peace")) {
+                    done = true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
